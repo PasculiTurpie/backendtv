6a93d79e29aaf2deee9c0a49d6390e8323116d99
Jorge R. Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2024-12-26 18:22:10 -0300

    feat:Configuracion de CORS 3

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const helmet = require("helmet");
-const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
 require("dotenv").config();
+
 const User = require("./routes/user.route");
 const Region = require("./routes/region.route");
 const Channel = require("./routes/channel.route");
@@ -12,16 +14,9 @@ const Multicast = require("./routes/multicast.route");
 const TipoMulticast = require("./routes/tipoMulticast.route");
 const Contact = require("./routes/contact.route");
 const { mongoose } = require("./config/config.mongoose");
-const morgan = require("morgan");
 
-/* const corsOptions = {
-  origin: 'https://example.com',
-  methods: 'GET,POST,PUT,DELETE',
-  allowedHeaders: 'Content-Type',
-}; */
-/* const cors = require("cors");
+const app = express();
 
-app.use(cors()); */
 const allowedCors = [
   "http://tv-operaciones.cl",
   "http://www.tv-operaciones.cl",
@@ -33,29 +28,28 @@ const allowedCors = [
   "http://localhost:8001/admin",
 ];
 
-app.use((req, res, next) => {
-  const { origin } = req.headers; // Obtener el origen de la solicitud
-  const { method } = req; // Obtener el método HTTP de la solicitud
-  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
-  if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
-  if (method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
-    const requestHeaders = req.headers["access-control-request-headers"];
-    res.header("Access-Control-Allow-Headers", requestHeaders);
-    return res.end();
-  }
-  next();
-});
+// Configuración de CORS
+const corsOptions = {
+  origin: (origin, callback) => {
+    // Permite solicitudes desde orígenes especificados o sin origen (por ejemplo, herramientas como Postman)
+    if (!origin || allowedCors.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+};
 
+app.use(cors(corsOptions)); // Integrar el middleware de CORS
 app.use(helmet());
 app.use(morgan("dev"));
-
 app.use(express.json());
 
 const PORT = process.env.PORT || 5001;
 
+// Rutas de la API
 app.use(
   "/api/v1",
   User,

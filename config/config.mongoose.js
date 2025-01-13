c401f00c80ae07c3a029037b9a0e1a3d77000dad
Jorge R. Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2025-01-13 11:39:11 -0300

    feat:Cambio de URL BD

--- config/config.mongoose.js
+++ config/config.mongoose.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose');
 
 const DB = process.env.PASSWORS_DB
 
-/* mongoose.connect('mongodb://localhost:27017/databaseTV')
+mongoose.connect('mongodb://localhost:27017/tv-operaciones')
 .then(() => {
     console.log('Connected to MongoDB');
-}) */
+})
 
-    mongoose.connect(`mongodb+srv://jsepulveda:${DB}@televisiondata.7mfoh.mongodb.net/`)
+    /* mongoose.connect(`mongodb+srv://jsepulveda:${DB}@televisiondata.7mfoh.mongodb.net/`)
     .then(() => {
         console.log('Connected to MongoDB.');
-    })
+    }) */
 
 
 .catch(err => {

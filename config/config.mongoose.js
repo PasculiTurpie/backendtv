93e04e4d44eed37a57e5d9d1df0ccd1e77df5386
Pasculi <rodrigo.s.turpie@gmail.com>
2025-01-27 09:18:52 -0300

    feat:Implementacion de actualización de contacto

--- config/config.mongoose.js
+++ config/config.mongoose.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose');
 
 const DB = process.env.PASSWORS_DB
 
-/* mongoose
+mongoose
   .connect("mongodb://localhost:27017/tv-operaciones")
   .then(() => {
     console.log("Connected to MongoDB");
-  }) */
+  })
 
-  mongoose.connect(`mongodb+srv://jsepulveda:${DB}@televisiondata.7mfoh.mongodb.net/`)
+  /* mongoose.connect(`mongodb+srv://jsepulveda:${DB}@televisiondata.7mfoh.mongodb.net/`)
     .then(() => {
         console.log('Connected to MongoDB.');
     })
-
+ */
   .catch((err) => {
     console.error("Error connecting to MongoDB", err);
   });

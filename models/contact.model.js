dbb006236cb36d5af922a777de7b3f2c626b7de1
Pasculi <rodrigo.s.turpie@gmail.com>
2025-01-27 13:31:34 -0300

    feat:Implementacion de actualización de contacto 3.18

--- models/contact.model.js
+++ models/contact.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+let uniqueValidator = require("mongoose-unique-validator");
 
 const ContactSchema = new mongoose.Schema(
   {
@@ -9,11 +10,12 @@ const ContactSchema = new mongoose.Schema(
     emailContact: {
       type: String,
       lowercase: true,
-      required: false,
+      unique: true, // El email será único
+      sparse: true, // Permite valores nulos/no definidos en índices únicos
       validate: {
         validator: function (v) {
           // Solo validar si se proporciona un valor
-          return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+          return !v || /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
         },
         message: "El email debe tener un formato válido",
       },
@@ -28,4 +30,5 @@ const ContactSchema = new mongoose.Schema(
 );
 
 const Contact = mongoose.model("Contact", ContactSchema);
+ContactSchema.plugin(uniqueValidator, { message: "{PATH} debe ser unico" });
 module.exports = Contact;

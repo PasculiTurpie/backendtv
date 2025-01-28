508a8245fdc4248edbde95d134e25fb1b73baf5e
Pasculi <rodrigo.s.turpie@gmail.com>
2025-01-28 13:31:52 -0300

    feat: Implementacion de controllers en Region

--- controllers/region.controller.js
+++ controllers/region.controller.js
@@ -10,6 +10,17 @@ module.exports.getRegions = async (req, res) => {
   }
 };
 
+module.exports.getRegionById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const region = await Region.findById(id);
+    if (!region) return res.status(404).json({ msg: "Región no encontrada" });
+    res.json(region);
+  } catch (error) {
+    res.status(500).json({ msg: "Error al obtener la región", error });
+  }
+}
+
 module.exports.createRegion = async (req, res) => {
   try {
     const { name } = req.body;
@@ -18,5 +29,26 @@ module.exports.createRegion = async (req, res) => {
     res.json(newRegion);
   } catch (error) {
     res.status(400).json({ msg: "Error al crear la región", error });
+  }
+}
+
+module.exports.updateRegion = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name } = req.body;
+    const updatedRegion = await Region.findByIdAndUpdate(id, { name }, { new: true });
+    res.json(updatedRegion);
+  } catch (error) {
+    res.status(400).json({ msg: "Error al actualizar la región", error });
+  }
+}
+
+module.exports.deleteRegion = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Region.findByIdAndDelete(id);
+    res.json({ msg: "Región eliminada exitosamente" });
+  } catch (error) {
+    res.status(500).json({ msg: "Error al eliminar la región", error });
   }
 }
--- routes/region.route.js
+++ routes/region.route.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const router = express.Router();
 const Region = require("../controllers/region.controller");
+const router = express.Router();
 
 router.get("/region", Region.getRegions);
+router.get("/region/:id", Region.getRegionById);
 router.post("/region", Region.createRegion);
+router.patch("/region/:id", Region.updateRegion);
+router.delete("/region/:id", Region.deleteRegion);
+
 
 module.exports = router;

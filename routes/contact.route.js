dd9fc79a5b2895785d2ef14d783108aebef009fa
PasculiTurpie <rodrigo.s.turpie@gmail.com>
2025-03-05 18:51:41 -0300

    feat:Endpoint router getIdContact

--- routes/contact.route.js
+++ routes/contact.route.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/contacto', Contact.getContact);
 router.post("/contacto", Contact.createContact);
+router.patch('/contacto/:id', Contact.getIdContact);
 router.patch('/contacto/:id', Contact.updateContact);
 
 
